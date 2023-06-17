c4ea28da9333f974eab45a970fba9d042a2e7cf0
Dhananjay <dhananjaychawda23@gmail.com>
2023-06-17 18:31:42 +0530

    Add Splash Screen

--- app/src/main/java/com/example/tracktransit/splash_screen.kt
+++ app/src/main/java/com/example/tracktransit/splash_screen.kt
@@ -1,11 +1,50 @@
 package com.example.tracktransit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 
 class splash_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val topAnim: Animation;
+        val bottomAnim: Animation;
+        val image: ImageView;
+        val logo: TextView;
+        val slogan: TextView;
+
+        //Hide the actionBar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+        supportActionBar?.hide()
+
+        //Animations
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image =findViewById(R.id.imageView);
+        logo =findViewById(R.id.textView);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+
+        //come second Acivity
+        Handler().postDelayed({
+            val intent= Intent(this@splash_screen,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+
+
+
     }
 }
